6b0d1ade70797e0b47887fd76e6fda075d51c19b
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-09-22 08:00:47 -0500

    Update uv.client.js

--- uv/uv.client.js
+++ uv/uv.client.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://infiniteknowledge.online/bare/',
+    bare: 'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
