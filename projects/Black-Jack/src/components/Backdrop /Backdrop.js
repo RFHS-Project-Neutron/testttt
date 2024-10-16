a2666c283f0b33ecbd829c6b071dcb4a1d17a7a0
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-10-16 16:24:04 -0500

    Delete projects/Black-Jack directory

--- projects/Black-Jack/src/components/Backdrop /Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Backdrop.module.css";
-
-const Backdrop = (props) => {
-  return props.show && <div className={styles.Backdrop}></div>;
-};
-
-export default Backdrop;
